d201e01bd51ad3caa08cbbeda0750ee4df4f5888
miguelpteix <miguelpteix@gmail.com>
2022-04-18 21:15:10 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,21 +3,23 @@ let espAltura = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  background('#fae');
   stroke(255);
   strokeWeight(2);
   noFill();
   background(0);
 }
 
-
 function draw() {
-  
-  for (let i = 1; i < espLarg; i++) {
+
+ for (let i = 1; i < espLarg; i++) {
     for (let j = 1; j < espAltura; j++) {
       push();
       translate(i * width / espLarg,j * height / espAltura)
-      circle(0, 0, height / espAltura);
+      translate(-40,-50);
+      scale(0.7);
+      circle(40, 50, height / espAltura);
+      line(40,50,40,100);
       pop();
     }
   }
